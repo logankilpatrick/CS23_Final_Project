768f92a453337c75ebaa6abad867f4c6dee6af70
Logan Kilpatrick <23kilpatrick23@gmail.com>
2019-04-27 10:38:39 -0700

    Custom FPS Controller is now working as well as the plane for the first game space.

---
+++ CS23_FInal/Assets/FPSController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float movementSpeed = 2f;
+    float FB_Move; //forward and backwords movement
+    float LR_Move; //Left right movement
+
+    public float viewSens = 15f;
+    float rotationX;
+    float rotationy;
+
+    CharacterController player;
+    public GameObject playerView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FB_Move = Input.GetAxis("Vertical") * movementSpeed;
+        LR_Move = Input.GetAxis("Horizontal") * movementSpeed;
+
+        rotationX = Input.GetAxis("Mouse X") * viewSens;
+        rotationy = Input.GetAxis("Mouse Y") * viewSens;
+
+
+        Vector3 movement = new Vector3(LR_Move, 0, FB_Move);
+        transform.Rotate(0, rotationX, 0);
+        playerView.transform.Rotate(-rotationy, 0, 0);
+        movement = transform.rotation * movement;
+
+        player.Move(movement * Time.deltaTime);
+    }
+}
