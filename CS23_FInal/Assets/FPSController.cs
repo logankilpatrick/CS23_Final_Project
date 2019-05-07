9375eb195bfe6fe4e0bafab021241c3029b94a57
Logan Kilpatrick <23kilpatrick23@gmail.com>
2019-05-06 20:17:28 -0700

    Physics is now working. Game over scene added. Need to be able to reset out of the game over scene next.

--- CS23_FInal/Assets/FPSController.cs
+++ CS23_FInal/Assets/FPSController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FPSController : MonoBehaviour
 {
@@ -15,6 +16,11 @@ public class FPSController : MonoBehaviour
     CharacterController player;
     public GameObject playerView;
 
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float numberOfLivesLeft = 3.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +30,42 @@ public class FPSController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FB_Move = Input.GetAxis("Vertical") * movementSpeed;
-        LR_Move = Input.GetAxis("Horizontal") * movementSpeed;
-
         rotationX = Input.GetAxis("Mouse X") * viewSens;
         rotationy = Input.GetAxis("Mouse Y") * viewSens;
 
+        if (player.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
 
-        Vector3 movement = new Vector3(LR_Move, 0, FB_Move);
-        transform.Rotate(0, rotationX, 0);
-        playerView.transform.Rotate(-rotationy, 0, 0); //makes the looking control not inverted. 
-        movement = transform.rotation * movement;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= movementSpeed;
 
-        player.Move(movement * Time.deltaTime);
+            transform.Rotate(0, rotationX, 0);
+            playerView.transform.Rotate(-rotationy, 0, 0); //makes the looking control not inverted. 
+            moveDirection = transform.rotation * moveDirection;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        //makes the looking control not inverted. 
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (player.transform.position.y < -10)
+        {
+            SceneManager.LoadScene("Game Over");
+            numberOfLivesLeft -= 1;
+        }
+
+        
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        player.Move(moveDirection * Time.deltaTime);
     }
+
+
 }
