dd4ae197be6415ef949d56ae4daf888129868c91
Logan Kilpatrick <23kilpatrick23@gmail.com>
2019-05-06 20:44:28 -0700

    returning to the play state after falling off the map and entering the game over state now working.

---
+++ CS23_FInal/Assets/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //This will launch the Play scene if we are in the game over scene and press enter/return.
+		if (Input.GetAxis("Submit") == 1) {
+			SceneManager.LoadScene("Play");
+		}
+	}
+}
